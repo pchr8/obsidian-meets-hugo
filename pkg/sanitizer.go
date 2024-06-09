99e217957686772a43024c1cc7f45b7d3c8aea5a
Serhii Hamotskyi <serhii@serhii.net>
2024-06-10 01:00:13 +0200

    Allow cyrillic letters in filenames

--- pkg/sanitizer.go
+++ pkg/sanitizer.go
@@ -2,7 +2,12 @@ package omh
 
 import "regexp"
 
-var insane = regexp.MustCompile(`[^a-zA-Z0-9\-]`)
+//var insane = regexp.MustCompile(`[^a-zA-Z0-9\-]`)
+// SH - add cyrillic characters to allowed
+// upper+lower:
+var insane = regexp.MustCompile(`[^a-zA-Z0-9\-\p{Cyrillic}]`)
+// var insane = regexp.MustCompile(`[^a-zA-Z0-9\\u0430-\\u044F-]`)
+
 
 func Sanitize(in string) string {
 	return insane.ReplaceAllString(in, "")
